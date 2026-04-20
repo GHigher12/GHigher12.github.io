6ec0504797d6bd3488ef4b101d17354f9e0d2960
GHigher12 <81691559+GHigher12@users.noreply.github.com>
2026-04-20 12:00:16 +0800

    Site updated: 2026-04-20 11:59:54

--- love/index.js
+++ love/index.js
@@ -1,9 +1,6 @@
-var htmer_time = document.getElementById("htmer_time");
-var htmer_time_time = null;
-
 function setTime() {
   var create_time = Math.round(
-    new Date(Date.UTC(2026, 4, 11, 0, 0, 0)).getTime() / 1000
+    new Date(Date.UTC(2026, 3, 11, 0, 0, 0)).getTime() / 1000
   );
   var timestamp = Math.round(
     (new Date().getTime() + 8 * 60 * 60 * 1000) / 1000
@@ -20,7 +17,10 @@ function setTime() {
     " 分 " +
     currentTime[4] +
     " 秒";
-  htmer_time.innerHTML = currentTimeHtml;
+  var htmer_time = document.getElementById("htmer_time");
+  if (htmer_time) {
+    htmer_time.innerHTML = currentTimeHtml;
+  }
 }
 function secondToDate(second) {
   if (!second) {
@@ -49,8 +49,4 @@ function secondToDate(second) {
   return time;
 }
 
-if (htmer_time) {
-  htmer_time_time = setInterval(setTime, 1000);
-} else {
-  clearInterval(htmer_time_time);
-}
+setInterval(setTime, 1000);
